e9ee5391116da6feb20cc6795ba1e7ebd803d548
Creighton Gorai <creighton-gorai@users.noreply.github.com>
2023-10-15 08:57:24 -1000

    Saved high score through file editing

--- scoreboard.py
+++ scoreboard.py
@@ -5,7 +5,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", mode="r") as file:
+            self.saved_score = file.read()
+        self.high_score = int(self.saved_score)
         self.goto(0, 270)
         self.color("white")
         self.write(f"Score: {self.score}  High Score: {self.high_score}", False,
@@ -18,10 +20,6 @@ class Scoreboard(Turtle):
         self.write(f"Score: {self.score}  High Score: {self.high_score}", False,
                    align="center", font=('Arial', 20, 'normal'))
 
-    # def game_over (self):
-    #     self.goto(0, 0)
-    #     self.write("Game Over", False, align="center", font=('Arial', 20, 'normal'))
-
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
@@ -29,4 +27,5 @@ class Scoreboard(Turtle):
         self.clear()
         self.write(f"Score: {self.score}  High Score: {self.high_score}", False,
                    align="center", font=('Arial', 20, 'normal'))
-
+        with open("data.txt", mode="w") as file:
+            file.write(f"{self.high_score}")
